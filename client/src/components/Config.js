4946d4242bbc986ef783bb35ac3193fc4194cb81
arb1ona <arbiona.shahu@gmail.com>
2021-01-21 15:27:09 +0100

    Get Data from API_KEY

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,18 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+export const API_KEY = "777e4d7a9022fdf6c1972dea345badfb";
+export const API_URL = "https://api.themoviedb.org/3/";
+export const IMAGE_URL = "https://image.tmdb.org/t/p/";
+
+// TRENDING MOVIES
+// "https://api.themoviedb.org/3/trending/all/day?api_key=<<api_key>>"
+
+// GET MOVIE BY LATEST
+// "https://api.themoviedb.org/3/movie/latest?api_key=<<api_key>>&language=en-US"
+
+// GET MOVIE DETAIL
+// "https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US"
+
+// GET MOVIE REVIEWS
+// "https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key=<<api_key>>&language=en-US&page=1"
