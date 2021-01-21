1f10aa1b82899f0ce6c53942b96cd61bcc7f718c
arb1ona <arbiona.shahu@gmail.com>
2021-01-21 21:07:40 +0100

    Landing Page - 2. Put data fetched from Movie API into the STATE

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
 import { API_URL, API_KEY } from "../../Config";
 import { Typography, Row } from "antd";
 const { Title } = Typography;
 
 function LandingPage() {
+  const [Movie, setMovie] = useState([]);
+
   useEffect(() => {
     fetch(`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
       .then((response) => response.json())
       .then((response) => {
         console.log(response);
+        setMovie(response.results);
       });
   }, []);
 
