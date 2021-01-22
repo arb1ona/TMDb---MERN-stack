359662624c61e58b497ebf38d40fd8f9619f1763
arb1ona <arbiona.shahu@gmail.com>
2021-01-22 13:39:15 +0100

    MovieDetail Page 2. Put data fetched from Movie API into STATE

--- client/src/components/views/MovieDetailPage/MovieDetailPage.js
+++ client/src/components/views/MovieDetailPage/MovieDetailPage.js
@@ -3,12 +3,15 @@ import { useEffect, useState } from "react";
 import { API_URL, API_KEY, IMAGE_URL } from "../../Config";
 
 function MovieDetailPage(props) {
+  const [Movie, setMovie] = useState([]);
+
   useEffect(() => {
     const movieId = props.match.params.movieId;
     fetch(`${API_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`)
       .then((response) => response.json())
       .then((response) => {
         console.log(response);
+        setMovie(response);
       });
   }, []);
 
